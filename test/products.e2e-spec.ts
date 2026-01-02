cabe449ab1c2f4f4458abba74ca61a1df5d31ce7
Hilaricus Stephane <stephane_hilaricus@ens.univ-artois.fr>
2026-01-02 15:16:37 +0100

    test(e2e): ajout d'un test fonctionnel sur la route /products

--- test/products.e2e-spec.ts
+++ test/products.e2e-spec.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('ProductsController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    // On compile l'application entière (comme en prod)
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -18,12 +17,10 @@ describe('ProductsController (e2e)', () => {
   });
 
   afterAll(async () => {
-    // On ferme l'app après les tests pour libérer les ports
     await app.close();
   });
 
-  // Test réel : on appelle la route GET /products
   it('/products (GET) should return 200', () => {
-    return request(app.getHttpServer()).get('/products').expect(200); // On attend un succès (OK), même si la liste est vide []
+    return request(app.getHttpServer()).get('/products').expect(200);
   });
 });
